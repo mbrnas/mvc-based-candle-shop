46b3426c91e26e29f2d6dbdd7796ad31eb879c62
Matija Brnas <mb8828@rit.edu>
2023-02-16 00:06:49 +0100

    Major revamp

--- js/View/CandleView.js
+++ js/View/CandleView.js
@@ -1,79 +1,79 @@
 /**
- * Class that represents the application view. The view displays information 
- * contained in the model: type & color. The view does not obtain the information 
- * directly from the model, it uses the controller as a mediator which instructs 
- * it when and what to display. 
- * 
+ * Class that represents the application view. The view displays information
+ * contained in the model: type & color. The view does not obtain the information
+ * directly from the model, it uses the controller as a mediator which instructs
+ * it when and what to display.
+ *
  * The view holds references to all UI elements with which the user interacts with
  * AND for which the event-handling mechanism needs to be implemented.
  */
 export class CandleView {
-    constructor(){
-        this.candleForm = document.querySelector("#form-candle");
-        this.selectsDiv = document.querySelector("#div-selects");
-        this.selects = null;
-        this.candleDiv = document.querySelector("#div-candle");
+  constructor() {
+    this.candleForm = document.querySelector("#form-candle");
+    this.selectsDiv = document.querySelector("#div-selects");
+    this.selects = null;
+    this.candleDiv = document.querySelector("#div-candle");
+  }
+  /**
+   * Renders HTML select elements. The options are not loaded in the process,
+   * meaning that there are no Option elements as part of the select element.
+   *
+   * @param {Array} selectIDs - array of strings (select ids)
+   */
+
+  renderSelects(selectIDs) {
+    selectIDs.forEach((name) => {
+      let select = document.createElement("select");
+      select.setAttribute("id", name);
+      select.options.add(new Option(` -- Select a ${name} -- `, "undefined"));
+      this.selectsDiv.appendChild(select);
+    });
+
+    this.selects = this.selectsDiv.querySelectorAll("select");
+  }
+  /**
+   * Resets all next selects, selects that are siblings to the one defined by
+   * this method parameter.
+   *
+   * @param {type} selectID - the ID of the select which next siblings are going to be reset
+   */
+
+  resetNextSiblings(selectID) {
+    let select = this.selectsDiv.querySelector(`#${selectID}`);
+    let nextSelect = select.nextElementSibling;
+    while (nextSelect) {
+      nextSelect.length = 1;
+      nextSelect = nextSelect.nextElementSibling;
     }
-    /**
-     * Renders HTML select elements. The options are not loaded in the process,
-     * meaning that there are no Option elements as part of the select element.
-     *  
-     * @param {Array} selectIDs - array of strings (select ids)  
-     */
+  }
+  /**
+   * Adds options to a select.
+   *
+   * @param {String} selectID
+   * @param {Array} options - array of strings (option names)
+   */
 
-    renderSelects(selectIDs) {
-        selectIDs.forEach((name) => {
-            let select = document.createElement('select');
-            select.setAttribute("id", name);
-            select.options.add(new Option(` -- Select a ${name} -- `, 'undefined'));
-            this.selectsDiv.appendChild(select);
-        });
+  addOptions(selectID, options) {
+    let select = this.selectsDiv.querySelector(`#${selectID}`);
+    select.length = 1;
+    options.forEach((option) => {
+      select.options.add(new Option(option, option));
+    });
+  }
 
-        this.selects = this.selectsDiv.querySelectorAll('select');
-    }
-    /**
-     * Resets all next selects, selects that are siblings to the one defined by
-     * this method parameter.
-     * 
-     * @param {type} selectID - the ID of the select which next siblings are going to be reset
-     */
+  /**
+   * Renders the image based on the current selects' values.
+   *
+   * @returns {undefined}
+   */
+  renderCandle() {
+    let imgSrc = "media/";
 
-    resetNextSiblings(selectID) {
-        let select = this.selectsDiv.querySelector(`#${selectID}`);
-        let nextSelect = select.nextElementSibling;
-        while (nextSelect) {
-            nextSelect.length = 1;
-            nextSelect = nextSelect.nextElementSibling;
-        }
-    }
-    /**
-     * Adds options to a select.
-     * 
-     * @param {String} selectID
-     * @param {Array} options - array of strings (option names)
-     */
+    this.selects.forEach((select) => {
+      imgSrc += `${select.value}-`;
+    });
+    imgSrc = imgSrc.slice(0, -1) + ".png"; //remove the last character '-'.
 
-    addOptions(selectID, options) {
-        let select = this.selectsDiv.querySelector(`#${selectID}`);
-        select.length = 1;
-        options.forEach((option) => {
-            select.options.add(new Option(option, option));
-        });
-    }
-
-    /**
-     * Renders the image based on the current selects' values.
-     * 
-     * @returns {undefined}
-     */
-    renderCandle() {
-        let imgSrc = 'media/';
-
-        this.selects.forEach((select) => {
-            imgSrc += `${select.value}-`;
-        });
-        imgSrc = imgSrc.slice(0, -1) + '.png'; //remove the last character '-'.
-
-        this.candleDiv.querySelector('img').src = imgSrc;
-    }
+    this.candleDiv.querySelector("img").src = imgSrc;
+  }
 }
